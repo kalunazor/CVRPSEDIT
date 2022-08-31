f00cf9519ce64ea338a552654e9f8dbb880b3b82
kalunazor <kalunazor@gmail.com>
2022-08-31 21:50:28 +0100

    refined my get prediction method

--- camera_rps_edit.py
+++ camera_rps_edit.py
@@ -7,54 +7,58 @@ import random
 
 class CameraRPS:
     def __init__(self):
-        pass
+        self.choices = ['rock', 'paper', 'scissors', 'nothing']
+        self.model = load_model('keras_model.h5')
+        self.cap = cv2.VideoCapture(0)
+        self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
         #Initialize the user score and the computer score.
     
     def get_computer_choice(self):
-        pass
-        get_user_choice = input("get_user_choice 'R' for rock, 'P' for paper, 'S' for scissors\n")
-        get_user_choice = get_user_choice.upper()
-
-        get_computer_choice = random.choice(['R', 'P', 'S'])
-
-        if get_user_choice == get_computer_choice:
-            return (0, get_user_choice, get_computer_choice)
-
-        if get_winner(get_user_choice, get_computer_choice):
-            return (1, get_user_choice, get_computer_choice)
-
-        return (-1, get_user_choice, get_computer_choice)
-
+        comp_choice = random.choice(self.choices[0:3])
+        return comp_choice
+          
     def get_prediction(self):
-        pass
-        model = load_model('keras_model.h5')
-        cap = cv2.VideoCapture(0)
-        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
-        while True: 
-            ret, frame = cap.read()
-            resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-            image_np = np.array(resized_frame)
-            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-            data[0] = normalized_image
-            prediction = model.predict(data)
-            cv2.imshow('frame', frame)
-            # Press q to close the window
-            print(prediction[0][0])
-            print(prediction[0][1])
-            print(prediction[0][2])
-            print(prediction[0][3])
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        prediction = self.model.predict(self.data)
+        print(prediction)
+        max_index = np.argmax(prediction[0])
+        user_choice = self.choices[max_index]
+        print(user_choice)
+        
+        # Press q to close the window
+        return user_choice
 
     def get_winner(self):
-        pass
-        if (user_choice == 'R' and computer_choice == 'S') or (user_choice == 'S' and computer_choice == 'P') or (user_choice == 'P' and computer_choice == 'R'):
-            return True
-        return False
+        self.get_computer_choice()
+        self.get_prediction()
+        
+        # Pass user_choice and comp_choice as parameters to the get_winner method
+        user_choice = self
 
 def play_game():
     game = CameraRPS()
-    game.get_computer_choice()
-    game.get_prediction()
-    
+    game.model
+    game.cap
+
+    while True: 
+        image = game.data
+        print(image.shape)
+        ret, frame = game.cap.read()
+        game.get_prediction()
+
+        # TODO: THE IMAGE IS EMPTY, HOW DO I FIX THIS?
+        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+        game.data[0] = normalized_image
+        cv2.imshow('frame', frame)
+        game.get_computer_choice()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        # After the loop release the cap object
+        game.cap.release()
+        # Destroy all the windows
+        cv2.destroyAllWindows()
+
+play_game()
